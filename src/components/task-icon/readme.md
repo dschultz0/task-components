5ced0905bf20a065a6b55d58a0a2608ef38c7452
dschultz0 <dave@daveschultzconsulting.com>
2023-09-14 09:36:08 -0700

    Flesh out icons support Add support for a wide range of icons with most frequently used icons native to the library

--- src/components/task-icon/readme.md
+++ src/components/task-icon/readme.md
@@ -7,23 +7,28 @@
 
 ## Properties
 
-| Property | Attribute | Description | Type     | Default     |
-| -------- | --------- | ----------- | -------- | ----------- |
-| `icon`   | `icon`    |             | `string` | `undefined` |
+| Property | Attribute | Description | Type     | Default    |
+| -------- | --------- | ----------- | -------- | ---------- |
+| `icon`   | `icon`    |             | `string` | `"blank"`  |
+| `size`   | `size`    |             | `number` | `STANDARD` |
 
 
 ## Dependencies
 
 ### Used by
 
+ - [task-button](../task-button)
  - [task-input-multiselect](../task-input-multiselect)
  - [task-step](../task-step)
+ - [task-tooltip](../task-tooltip)
 
 ### Graph
 ```mermaid
 graph TD;
+  task-button --> task-icon
   task-input-multiselect --> task-icon
   task-step --> task-icon
+  task-tooltip --> task-icon
   style task-icon fill:#f9f,stroke:#333,stroke-width:4px
 ```
 
--- src/components/task-icon/task-icon.tsx
+++ src/components/task-icon/task-icon.tsx
@@ -1,4 +1,9 @@
 import { Component, Host, h, Prop } from '@stencil/core';
+import icons from './icons'
+import classNames from 'classnames';
+
+const STANDARD = 16
+const LARGE = 20
 
 @Component({
   tag: 'task-icon',
@@ -6,30 +11,43 @@ import { Component, Host, h, Prop } from '@stencil/core';
   scoped: true,
 })
 export class TaskIcon {
-  @Prop() icon: string
+  @Prop() icon: string = "blank"
+  @Prop() size: number = STANDARD
 
   iconSVG() {
-    switch(this.icon) {
-      case "blank":
-        return <svg data-icon="blank" height="16" role="img" viewBox="0 0 16 16" width="16"></svg>
-      case "small-tick":
-        return <svg data-icon="small-tick" height="16" role="img" viewBox="0 0 16 16" width="16"><path d="M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z" fill-rule="evenodd"></path></svg>
-      case "caret-down":
-        return <svg data-icon="caret-" height="16" role="img" viewBox="0 0 16 16" width="16"><path d="M12 6.5c0-.28-.22-.5-.5-.5h-7a.495.495 0 00-.37.83l3.5 4c.09.1.22.17.37.17s.28-.07.37-.17l3.5-4c.08-.09.13-.2.13-.33z" fill-rule="evenodd"></path></svg>
-      case "caret-left":
-        return <svg data-icon="caret-" height="16" role="img" viewBox="0 0 16 16" width="16"><path d="M9.5 4c-.13 0-.24.05-.33.13l-4 3.5c-.1.09-.17.22-.17.37s.07.28.17.37l4 3.5a.495.495 0 00.83-.37v-7c0-.28-.22-.5-.5-.5z" fill-rule="evenodd"></path></svg>
-      case "caret-right":
-        return <svg data-icon="caret-" height="16" role="img" viewBox="0 0 16 16" width="16"><path d="M11 8c0-.15-.07-.28-.17-.37l-4-3.5A.495.495 0 006 4.5v7a.495.495 0 00.83.37l4-3.5c.1-.09.17-.22.17-.37z" fill-rule="evenodd"></path></svg>
-      case "caret-up":
-        return <svg data-icon="caret-" height="16" role="img" viewBox="0 0 16 16" width="16"><path d="M11.87 9.17s.01 0 0 0l-3.5-4C8.28 5.07 8.15 5 8 5s-.28.07-.37.17l-3.5 4a.495.495 0 00.37.83h7a.495.495 0 00.37-.83z" fill-rule="evenodd"></path></svg>
-    }
+    const pathSize = this.size < LARGE ? STANDARD : LARGE
+    return <svg
+      data-icon={this.icon}
+      height={this.size}
+      width={this.size}
+      role="img"
+      viewBox={`0 0 ${pathSize} ${pathSize}`}
+      >
+      {icons[this.icon][pathSize].map(p => <path d={p} fill-rule="evenodd"></path>)}
+    </svg>
   }
+
+  iconFont() {
+    const sizeClass = this.size < LARGE ? "icon-standard" : "icon-large"
+    const classes = classNames(sizeClass, `icon-${this.icon}`)
+    let style = {}
+    if (![16,20].includes(this.size)) {
+      const size = `${this.size}px`
+      style = {
+        "font-size": size,
+        height: size,
+        width: size
+      }
+    }
+    return <span class={classes} style={style}></span>
+  }
+
   render() {
     return (
-      <Host>
-        {this.iconSVG()}
+      // TODO: Figure out why this size override is necessary
+      <Host class="icon" style={{height: `${this.size}px`}}>
+        {this.icon in icons ? this.iconSVG() : this.iconFont()}
       </Host>
     );
   }
-
 }

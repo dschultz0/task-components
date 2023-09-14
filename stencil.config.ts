bc58f687fc4fefd9a256c5033c96cbd8fee29863
dschultz0 <dave@daveschultzconsulting.com>
2023-09-14 09:23:52 -0700

    Add config for icon fonts and global css

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,8 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'task-components',
+  globalStyle: 'src/global/global.css',
+  buildEs5: 'prod',
   outputTargets: [
     {
       type: 'dist',
@@ -9,13 +11,30 @@ export const config: Config = {
     },
     {
       type: 'dist-custom-elements',
+      copy: [
+        {
+          src: 'src/assets/*.woff2',
+          dest: 'dist/assets',
+          warn: true
+        }
+      ]
     },
     {
       type: 'docs-readme',
+      dir: 'docs'
     },
     {
       type: 'www',
       serviceWorker: null, // disable service workers
+      copy: [
+        {
+          src: 'global'
+        }
+      ]
     },
-  ],
+    {
+      type: 'docs-vscode',
+      file: 'vscode-data.json'
+    },
+  ]
 };
